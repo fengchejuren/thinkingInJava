cca2f4a54f168df69395a86cf299af93b90c8dff
superman <superman166@126.com>
2014-07-26 17:33:59 +0800

    动态代理与混合模式

--- src/generics/DynamicProxyMixin.java
+++ src/generics/DynamicProxyMixin.java
@@ -1,5 +1,27 @@
 package generics;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.util.Map;
+
+class Mixin implements InvocationHandler{
+
+	Map<String, Object> delegatesByMethod;
+	@Override
+	public Object invoke(Object arg0, Method arg1, Object[] arg2)
+			throws Throwable {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
+
+/** 可以使用动态代理来实现一种比装饰器模式更贴近混合模式的机制
+ * @ClassName: DynamicProxyMixin 
+ * @Description: TODO(这里用一句话描述这个类的作用) 
+ * @author Rock King
+ * @date 2014年7月26日 上午11:31:58  
+ */ 
 public class DynamicProxyMixin {
 
 }
