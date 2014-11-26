c167e1e8a2fab12d0b6e4c81367a73161758829e
fengchejuren <376636507@qq.com>
2014-11-26 19:03:35 +0800

    21.4终结任务 前两个section

---
+++ src/concurrency/section4/Interrupting.java
@@ -0,0 +1,87 @@
+package concurrency.section4;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+class SleepBlocked implements Runnable{
+
+	public void run() {
+		try {
+			TimeUnit.SECONDS.sleep(100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Exiting SleepBlocked.run()--- ");
+	}
+	
+}
+
+class IOBlocked implements Runnable{
+	private InputStream in;
+	public IOBlocked(InputStream is){
+		this.in = is;
+	}
+	public void run() {
+		try {
+			System.out.println("Waiting for read:");
+			in.read();
+		} catch (IOException e) {
+			if(Thread.currentThread().isInterrupted())
+				System.out.println("Interrupted from blocked I/O");
+			else 
+				throw new RuntimeException(e);
+		}
+		System.out.println("Exiting IOBlocked.run()---");
+	}
+	
+}
+
+class SynchronizedBlocked implements Runnable{
+
+	public synchronized void f(){
+		while(true)
+			Thread.yield();
+	}
+	public SynchronizedBlocked() {
+		new Thread(){
+			public void run() {
+				f();
+			};
+		}.start();
+	}
+	public void run() {
+		System.out.println("Trying to call f()");
+		f();
+		System.err.println("Exiting SynchronizedBlocked.run()---");
+	}
+	
+}
+
+public class Interrupting {
+
+	private static ExecutorService exec = Executors.newCachedThreadPool();
+	static void test(Runnable r) throws InterruptedException{
+		Future<?> future = exec.submit(r);
+		TimeUnit.MILLISECONDS.sleep(100);
+		System.out.println("Interruping "+r.getClass().getName());
+		future.cancel(true);
+		System.out.println("Interrupt to "+r.getClass().getName());
+	}
+	
+	public static void main(String[] args) throws InterruptedException {
+		test(new SleepBlocked());
+		test(new IOBlocked(System.in));
+		test(new SynchronizedBlocked());
+		TimeUnit.SECONDS.sleep(3);
+		System.out.println("Aborting with System.exit(0) ");
+		System.exit(0);
+	}
+	
+	
+	
+	
+}
